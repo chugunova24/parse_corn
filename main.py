d6a030e6de2f25c774cce04bdb7120ad51c5260e
Танюха <80966519+chugunova24@users.noreply.github.com>
2021-11-24 12:21:00 +0300

    изменения 1

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import telebot
-import telebot.types as types
 import datetime
 import re
 from babel.dates import format_date, format_datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+import pprint
 
-#работа браузера без интерфейса
+# работа браузера без интерфейса
 option = Options()
 option.headless = False
 
@@ -20,6 +20,7 @@ x = datetime.datetime.today()
 x2 = datetime.datetime.date(x)
 date_today = format_date(x2, locale='de_DE')
 
+
 # последовательность
 @bot.message_handler(commands=['news_today'])
 def answer(message):
@@ -31,6 +32,7 @@ def answer(message):
             ПОИСК = '5'
             ''')
     bot.register_next_step_handler(send, get_news)
+
 
 def get_news(message):
     a = message.text
@@ -78,6 +80,7 @@ def zerno_ru():
 
     return message_text
 
+
 # вывод сообщения в чат и zol_ru(message)
 def zol_ru():
     url = 'https://www.zol.ru/news/grain/'
@@ -114,7 +117,7 @@ def zol_ru():
         a_link = j.get_attribute('href')
         news_link_arr.append(a_link)
     # print(news_link_arr)
-    index_elem-=1
+    index_elem -= 1
     del news_link_arr[index_elem:]
 
     message_text = ["%s %02s" % t for t in zip(cell_news_arr, news_link_arr)]
@@ -145,11 +148,11 @@ def agroinvestor_ru():
         a_link = j.get_attribute('href')
         news_link_arr.append(a_link)
 
-
     message_text = ["%s %02s" % t for t in zip(cell_news_arr, news_link_arr)]
     return message_text
     # for x in message_text:
     #     bot.send_message(message.chat.id, x)
+
 
 def agriculture_com():
     url = 'https://www.agriculture.com/search?search_api_views_fulltext=&sort_by=created'
@@ -184,74 +187,76 @@ def agriculture_com():
 
 # добавление ключевого слова
 def dict_new_word(message):
-   f = open('dict.txt', 'a', encoding='utf-8')
-   word = 'бляьб1111'
-   f.write('\n')
-   f.write(word + ' ')
-   f.close
+    f = open('dict.txt', 'a', encoding='utf-8')
+    word = 'бляьб1111'
+    f.write('\n')
+    f.write(word + ' ')
+    f.close
 
 
 # добавление синонимов к ключевому слову
 def synonym(message):
-   f = open('dict.txt', 'a', encoding='utf-8')
-   word = ['сука', 'ааа', 'нееееет']
-   for elem in word:
-      f.write(elem + ' ')
-   f.close
+    f = open('dict.txt', 'a', encoding='utf-8')
+    word = ['сука', 'ааа', 'нееееет']
+    for elem in word:
+        f.write(elem + ' ')
+    f.close
+
 
 # чтение последней строки, чтобы показать результат запроса
 def print_last_line(message):
-   f = open('dict.txt', 'r', encoding='utf-8')
-   last_line = f.readlines()[-1]
-   f.close
-   return last_line
+    f = open('dict.txt', 'r', encoding='utf-8')
+    last_line = f.readlines()[-1]
+    f.close
+    return last_line
 
 
 # цикл чтения словаря, для поиска ключевого слова
 def read_txt(message):
-   message_lower_text = message.text.lower()
-   try:
-      n1 = zerno_ru()
-      n2 = zol_ru()
-      a = n1 + n2
-      return a
-   finally:
-       print('\n ПАРСЕР ЗАКОНЧИЛ РАБОТУ')
-      # print('ПОЛНЫЙ СПИСОК', a)
-   # except Exception:
-   #    print('ОШИБКА ПОИСКА')
+    message_lower_text = message.text.lower()
+    print('\nмеседж ловер')
+    try:
+        n1 = zerno_ru()
+        n2 = zol_ru()
+        a = n1 + n2
+        # print('ПОЛНЫЙ СПИСОК', a)
+    except Exception:
+       print('ОШИБКА ПОИСКА')
 
-   # -------------- КЛЮЧ -----------------
-   key = '%s ' % message_lower_text
-   f = open('dict.txt', 'r', encoding='utf-8')
-   f_lines = f.readlines()
+    key = f'{message_lower_text} '
+    print('\nключ найден')
+    f = open('dict.txt', 'r', encoding='utf-8')
+    f_lines = f.readlines()
 
-   for line in f_lines:
-       if key in line:
-           print(line)
-           return line
+    txt_line = ""
+    for line in f_lines:
+        print(line)
+        print(key)
+        if line.find(key) != -1:
+            txt_line = line.split(' ')
+            print('говно найдено', txt_line)
+    if not txt_line:
+        bot.send_message(message.chat.id, '')
 
-   # поиск в файле строки с нужным набором слов (выход str)
-   # for word in line:
-   #     if re.search(r'\bзвать\b', 'меня звать Олег, мне 35 лет'):
-   #         print("Привет")
+    # print('\nстроки прочитаны')
+    # поиск в файле строки с нужным набором слов (выход str)
 
-   result = []
-   for str_a in a:
-       for word in line:
-           if re.search(r'\b%s\b', 'меня звать Олег, мне 35 лет') % word:
-               result.append(line)
-   print(result)
-
-
-
-
-
-
+    result = []
+    for str_a in a:
+        bul_p = False
+        for word in txt_line:
+            print(word)
+            if bul_p == False:
+                if str_a.find(word + ' ') != -1:
+                    result.append(str_a)
+                    bul_p = True
 
 
+
+    print('\nрезультат итоговый')
+    r_set = set(result)
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(r_set)
 
 
 bot.infinity_polling()
-
-
