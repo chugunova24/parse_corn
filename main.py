c3cd9119ca25fad236012bca108932309ac5cc7d
Танюха <80966519+chugunova24@users.noreply.github.com>
2021-12-08 22:17:29 +0300

    изменения 6. проблемы с поиском

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import telebot
+from telebot import types
 import datetime
 import os
-import re
-from telebot import types
 from babel.dates import format_date, format_datetime
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 import pprint
 import asyncio
 from bs4 import BeautifulSoup
 import requests
 from urllib.request import urlopen
 from lxml import etree
+<<<<<<< HEAD
+import re
+=======
+# govno
+>>>>>>> origin/master
 
-# работа браузера без интерфейса
-option = Options()
-option.headless = False
-
-driver = webdriver.Chrome(options=option)
-
+# # работа браузера без интерфейса
+# option = Options()
+# option.headless = False
+#
+# driver = webdriver.Chrome(options=option)
 
 TOKEN = '2016761889:AAF0Baan6ouhKLwClb0O4utKv47wnclaZEA'
 bot = telebot.TeleBot(TOKEN)
@@ -72,7 +72,7 @@ def callback_inline(call):
                bot.send_message(call.message.chat.id, "Введите слово для поиска.")
                bot.register_next_step_handler(call.message, FindNews)
            if call.data == "AddSynonim":
-               send = bot.send_message(call.message.chat.id, "Введите слово к которому добавляем синоним.")
+               send = bot.send_message(call.message.chat.id, "Введите слово, к которому добавляем синоним.")
                bot.register_next_step_handler(send, AddSynonimTo)
    except Exception as e:
        print(repr(e))
@@ -82,12 +82,12 @@ def AddSynonimTo(SynTo):
     SynToGlobal = ""
 
     if SynToGlobal.find(' ') != -1:
-         send = bot.send_message(SynTo.chat.id, "похоже что вы ввели не одно слово, повторите пожалуйста")
+         send = bot.send_message(SynTo.chat.id, "Похоже, что вы ввели не одно слово, повторите пожалуйста")
          bot.register_next_step_handler(send, AddSynonimTo)
 
     SynToGlobal = SynTo.text.lower()
     print(SynToGlobal)
-    send = bot.send_message(SynTo.chat.id, "Отлично теперь введите синосим")
+    send = bot.send_message(SynTo.chat.id, "Отлично, теперь введите синосим")
     bot.register_next_step_handler(send, AddSynonimWord)
 
 def AddSynonimWord(SynWord):
@@ -110,7 +110,7 @@ def AddSynonimWord(SynWord):
                 f.write(line + " " + SynWordText)
             else:
                 f.write(line)
-            print('говно найдено')
+            print('строка в словаре найдена')
             HavWordToSyn = True
         else:
             f.write(line)
@@ -136,35 +136,7 @@ def AddSynonimWord(SynWord):
 # @bot.message_handler(commands='z1')
 async def zerno_ru():
 
-    # ЧЕРЕЗ SELENIUM
-
-    # url = 'https://zerno.ru/news_list'
-    # driver.get(url)
-    #
-    # xPATH = '''//*[text()='%s']''' % date_today
-    # xPATH_link = '''//*[text()='%s']//../../span[2]/span/a''' % date_today
-    #
-    # # ищет новости на сегодня
-    # cell_news_arr = []
-    # cell_news = driver.find_elements(By.XPATH, xPATH_link)
-    # for i in cell_news:
-    #     cell_news_arr.append(i.text)
-    #
-    # # поиск ссылок
-    # news_link_arr = []
-    # news_link = driver.find_elements(By.XPATH, xPATH_link)
-    # for j in news_link:
-    #     a_link = j.get_attribute('href')
-    #     news_link_arr.append(a_link)
-    #
-    # message_text = ["%s %02s" % t for t in zip(cell_news_arr, news_link_arr)]
-    # # print(message_text)
-    # # for x in message_text:
-    # #     bot.send_message(message.chat.id, x)
-    #
-    # return message_text
-
-    # ЧЕРЕЗ BS4
+    # date_today = '03.12.2021'
     xPATH = '''//*[text()='%s']//../../span[2]/span/a''' % date_today
     xPATH_link = '''//*[text()='%s']//../../span[2]/span/a/@href''' % date_today
 
@@ -178,22 +150,13 @@ async def zerno_ru():
     dom = etree.HTML(str(soup))
     count_index = dom.xpath(xPATH)
     # count_index = len(count_index)
-    # bukva = True
-    # if bukva == True:
-    #     for index:
-    #         index =+1
-    #         print(dom.xpath(xPATH_link)[index].text)
 
     # поиск содержимого блоков
     cell_news_arr = []
     for i in range(0, len(count_index)):
         a = dom.xpath(xPATH)[i].text
         cell_news_arr.append(a)
-    print(cell_news_arr)
-
-    # for x in cell_news_arr:
-    #     bot.send_message(918497328, x)
-    # bot.send_message(message.chat.id, cell_news_arr)
+    # print(cell_news_arr)
 
     # поиск ссылок на содержимое блоков
     news_link_arr = []
@@ -201,112 +164,232 @@ async def zerno_ru():
             a = dom.xpath(xPATH_link)[i]
             news_link_arr.append(a)
     # print(news_link_arr)
+    full_links = []
+    for i in news_link_arr:
+        first_part_link = 'https://zerno.ru'
+        i = first_part_link + i
+        full_links.append(i)
 
-    return cell_news_arr
+    message_text = ["%s %02s" % t for t in zip(cell_news_arr, full_links)]
+    # print(message_text)
 
 
+    return message_text
 
-# вывод сообщения в чат и zol_ru(message)
+
 async def zol_ru():
-    url = 'https://www.zol.ru/news/grain/'
-    driver.get(url)
 
+    # BS4
     if datetime.date.today().weekday() == 0:
         yesterday = datetime.datetime.now() - datetime.timedelta(2)
     else:
         yesterday = datetime.datetime.now() - datetime.timedelta(1)
-
     date_today = format_date(yesterday, "d MMMM yyy", locale='ru')
+    # date_today = '2 декабря 2021'
 
+    URL = 'https://www.zol.ru/news/grain/'
     xPATH = '''//tr'''
-    xPATH_link = '''//tr/td[2]//div/a'''
+    xPATH_link = '''//a[@class='news_block']'''
 
-    cell_news = driver.find_elements(By.XPATH, xPATH)
-    cell_news_arr = []
-    for cell in cell_news:
-        cell_news_arr.append(cell.text)
 
-    # В ПОНЕДЕЛЬНИК МОЖЕТ БЫТЬ ОШИБКА!!kjbjbjnlklmk;l;'
+    webpage = requests.get(URL)
+    soup = BeautifulSoup(webpage.text, 'lxml')
+    quotes = soup.findAll('td')
+    # print(quotes)
+
+    mass = []
+    for quote in quotes:
+        # print(quote.text)
+        mass.append(quote.text)
+    # print(mass)
+
+    pre_mass = []
+    for i in mass:
+        i = re.sub("^\s+|\n|\r|\s+$", '', i)
+        pre_mass.append(i)
+    # print(pre_mass)
+
+
+    # поиск индекс строки с датой, проверка массива на пустоту
+    # print(date_today)
     try:
-        index_elem = cell_news_arr.index(date_today)
+        index_elem = pre_mass.index(date_today)
     except ValueError:
-        # bot.send_message(message.chat.id, 'На сайте https://www.zol.ru/ (2) нет сегодня новостей!')
         print('На сайте https://www.zol.ru/ (2) нет сегодня новостей!')
         return
+    del pre_mass[index_elem:]
+    del pre_mass[0]
+    # print(pre_mass)
 
-    del cell_news_arr[index_elem:]
-    del cell_news_arr[0]
+    for i in pre_mass:
+        if len(i) == 5:
+            pre_mass.remove(i)
+    # print(pre_mass)
 
-    news_link_arr = []
-    news_link = driver.find_elements(By.XPATH, xPATH_link)
-    for j in news_link:
-        a_link = j.get_attribute('href')
-        news_link_arr.append(a_link)
-    # print(news_link_arr)
-    index_elem -= 1
-    del news_link_arr[index_elem:]
+    # # поиск ссылок на содержимое блоков
+    link_mass = soup.findAll('a', class_='news_block')
+    # print(link_mass)
 
-    message_text = ["%s %02s" % t for t in zip(cell_news_arr, news_link_arr)]
-    return message_text
+    l_mass = []
+    for i in link_mass:
+        i = i.get('href')
+        l_mass.append(i)
+
+    # подсчитаем кол-во новостей за сегодня и сравняем кол-во ссылок
+    len_pre_mass = len(pre_mass)
+    # print(len_pre_mass)
+    len_l_mass = len(l_mass)
+    # print(len_l_mass)
+    link = l_mass[:len_pre_mass]
+    # print(len(link))
+
+    part_link_part = '''https://www.zol.ru'''
+    link_news = []
+    for i in link:
+        i = part_link_part + i
+        link_news.append(i)
+    # print(link_news)
+
+    link_mass = []
+    for link in link_news:
+        # link = re.sub(r'\b-', '\-', link)
+        # print(link)
+        link1 = '<a href="%s">zol.ru</a>' % link
+        # print(link1)
+        link_mass.append(link1)
+
+    message_text = ["%s %02s" % t for t in zip(pre_mass, link_mass)]
+    # print(message_text)
+
     # for x in message_text:
-    #     bot.send_message(message.chat.id, x)
+    #     bot.send_message(message.chat.id, x, disable_web_page_preview=True, parse_mode='html')
+
+    return message_text
+
 
 
 async def agroinvestor_ru():
-    url = 'https://www.agroinvestor.ru/'
-    driver.get(url)
 
-    date_today = format_date(x2, "d MMMM yy", locale='ru')
-    # date_today = '29 октября 2021'
-    xPATH = '''//*[text()='%s']/../../a[2]''' % date_today
+    # BS4
+    URL = 'https://www.agroinvestor.ru/'
 
-    # поиск новостей
-    cell_news = driver.find_elements(By.XPATH, xPATH)
+    date_today = format_date(x2, "d MMMM yyyy", locale='ru')
+    # date_today = '2 декабря 2021'
+
+    xPATH = '''//*[text()='%s']/../../a/h3''' % date_today
+    xPATH_link = '''//*[text()='%s']/../../a[2]/@href''' % date_today
+
+    webpage = requests.get(URL)
+    soup = BeautifulSoup(webpage.content, "html.parser")
+    dom = etree.HTML(str(soup))
+    count_index = dom.xpath(xPATH)
+
+
+    # поиск содержимого блоков
     cell_news_arr = []
-    for cell in cell_news:
-        cell_news_arr.append(cell.text)
+    for i in range(0, len(count_index)):
+        a = dom.xpath(xPATH)[i].text
+        cell_news_arr.append(a)
     # print(cell_news_arr)
 
-    # поиск ссылок
+    # поиск ссылок на содержимое блоков
     news_link_arr = []
-    news_link = driver.find_elements(By.XPATH, xPATH)
-    for j in news_link:
-        a_link = j.get_attribute('href')
-        news_link_arr.append(a_link)
+    for i in range(0, len(count_index)):
+        a = dom.xpath(xPATH_link)[i]
+        news_link_arr.append(a)
+    # print(news_link_arr)
 
-    message_text = ["%s %02s" % t for t in zip(cell_news_arr, news_link_arr)]
-    return message_text
+
+    first_part_link = 'https://www.agroinvestor.ru'
+    link_news = []
+    for i in news_link_arr:
+        i = first_part_link + i
+        link_news.append(i)
+    # print(link_news)
+
+    # преобразование ссылок в нужный вид
+    link_mass = []
+    for link in link_news:
+        # link = re.sub(r'\b-', '\-', link)
+        # print(link)
+        link1 = '<a href="%s">agroinvestor.ru</a>' % link
+        print(link1)
+        link_mass.append(link1)
+
+    message_text = ["%s %02s" % t for t in zip(cell_news_arr, link_mass)]
+
     # for x in message_text:
-    #     bot.send_message(message.chat.id, x)
+    #     bot.send_message(message.chat.id, x, disable_web_page_preview=True, parse_mode='html')
+
+    return message_text
 
 
 async def agriculture_com():
-    url = 'https://www.agriculture.com/search?search_api_views_fulltext=&sort_by=created'
-    driver.get(url)
 
-    # date_today = '10.31.2021'
+    # BS4
+
+    URL = 'https://www.agriculture.com/search?search_api_views_fulltext=&sort_by=created'
+
+    # date_today = '12.07.2021'
     date_today = format_date(x2, "MM.dd.yyy", locale='ru')
-    xPATH = '''//*[contains(text(),'%s')]/../h2''' % date_today
-    xPATH_link = '''//*[contains(text(),'%s')]/../h2/a''' % date_today
+    xPATH = '''//*[contains(text(),'%s')]/../h2/a''' % date_today
+    xPATH_link = '''//*[contains(text(),'%s')]/../h2/a/@href''' % date_today
 
-    # поиск новостей
-    cell_news = driver.find_elements(By.XPATH, xPATH)
+    webpage = requests.get(URL)
+    soup = BeautifulSoup(webpage.content, "html.parser")
+    dom = etree.HTML(str(soup))
+    count_index = dom.xpath(xPATH)
+    # count_index = len(count_index)
+
+    # поиск содержимого блоков НА 1 СТРАНИЦЕ
     cell_news_arr = []
-    for cell in cell_news:
-        cell_news_arr.append(cell.text)
+    for i in range(0, len(count_index)):
+        a = dom.xpath(xPATH)[i].text
+        cell_news_arr.append(a)
+    # print(cell_news_arr)
 
-    # поиск ссылок
+    # поиск ссылок на содержимое блоков
     news_link_arr = []
-    news_link = driver.find_elements(By.XPATH, xPATH_link)
-    for j in news_link:
-        a_link = j.get_attribute('href')
-        news_link_arr.append(a_link)
+    for i in range(0, len(count_index)):
+        a = dom.xpath(xPATH_link)[i]
+        news_link_arr.append(a)
 
-    message_text = ["%s %02s" % t for t in zip(cell_news_arr, news_link_arr)]
-    return message_text
+    # поиск содержимого блоков НА СЛЕДУЮЩИХ СТРАНИЦАХ
+    def drug_data(page_number):
+        URL = 'https://www.agriculture.com/search?search_api_views_fulltext=&sort_by=created&page=' + str(page_number)
+        print(URL)
+        webpage = requests.get(URL)
+        soup = BeautifulSoup(webpage.content, "html.parser")
+        dom = etree.HTML(str(soup))
+        count_index = dom.xpath(xPATH)
+
+        for i in range(0, len(count_index)):
+            a = dom.xpath(xPATH)[i].text
+            cell_news_arr.append(a)
+
+        for i in range(0, len(count_index)):
+            a = dom.xpath(xPATH_link)[i]
+            news_link_arr.append(a)
+
+    for x in range(1, 3):
+        drug_data(x)
+
+    # print(cell_news_arr)
+    # print(news_link_arr)
+
+    # преобразование ссылок в нужный вид
+    link_mass = []
+    for link in news_link_arr:
+        link1 = '<a href="%s">agriculture.com</a>' % link
+        link_mass.append(link1)
+
+    message_text = ["%s %02s" % t for t in zip(cell_news_arr, link_mass)]
+    # print(message_text)
+
     # for x in message_text:
-    #     bot.send_message(message.chat.id, x)
+    #     bot.send_message(message.chat.id, x, disable_web_page_preview=True, parse_mode='html')
 
+    return message_text
 
 # -------------- СЛОВАРЬ -----------------
 def FindNews(mess):
@@ -314,22 +397,22 @@ def FindNews(mess):
 # цикл чтения словаря, для поиска ключевого слова
 async def asyncFindNews(message):
     task1 = asyncio.create_task(zerno_ru())
-    # task2 = asyncio.create_task(zol_ru())
-    # task3 = asyncio.create_task(agroinvestor_ru())
-    # task4 = asyncio.create_task(agriculture_com())
-    # await asyncio.wait([task1,task2,task3,task4])
-    await asyncio.wait([task1])
+    task2 = asyncio.create_task(zol_ru())
+    task3 = asyncio.create_task(agroinvestor_ru())
+    task4 = asyncio.create_task(agriculture_com())
+    await asyncio.wait([task1,task2,task3,task4])
+    # await asyncio.wait([task1])
 
     message_words = message.text.lower().split(' ')
     print('\nмеседж ловер')
     try:
         n1 = task1.result()
-        # n2 = task2.result()
-        # n3 = task3.result()
-        # n4 = task4.result()
-        # a = n1 + n2 + n3 + n4
-        a = n1
-        # print('ПОЛНЫЙ СПИСОК', a)
+        n2 = task2.result()
+        n3 = task3.result()
+        n4 = task4.result()
+        a = n1 + n2 + n3 + n4
+        # a = n1 + n2
+        print('ПОЛНЫЙ СПИСОК', a)
     except Exception:
        print('ОШИБКА ПОИСКА')
 
@@ -356,24 +439,27 @@ async def asyncFindNews(message):
     ParseResult = a
     TempParseResult = []
 
-    print("ParseResult")
-    print(ParseResult)
+    # print("ParseResult")
+    # print(ParseResult)
     for WordLine in WordsLineWithSyn:
         wordFinded = False
         for word in WordLine.split(" "):
             if wordFinded == False:
                 for ParseNews in ParseResult:
-                    if ParseNews.lower().replace(':', ' ').replace('.', ' ').replace(',', ' ').replace('"', ' ')\
+                    if ParseNews.lower().replace(':', ' ').replace('.', ' ').replace(',', ' ').replace('"', ' ').replace('/', ' ')\
                             .find(word + " ") != -1:
                         TempParseResult.append(ParseNews)
                         wordFinded = True
         ParseResult = TempParseResult
         TempParseResult = []
 
-    print(ParseResult)
+    # print(ParseResult)
 
     for elem_r_set in ParseResult:
-        bot.send_message(message.chat.id, elem_r_set)
+        # bot.send_message(message.chat.id, elem_r_set)
+        bot.send_message(message.chat.id, elem_r_set, disable_web_page_preview=True, parse_mode='html')
+
+
 
 
 
